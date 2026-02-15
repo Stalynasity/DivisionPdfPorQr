401984e42d20ffe873df2f9112b07b8c0ea0ef99
stalynAsiti <stalynasi08@gmail.com>
2026-02-15 15:09:56 -0500

    rename

--- src/config/tenants.js
+++ src/config/tenants.js
@@ -3,16 +3,15 @@ import path from "path";
 dotenv.config();
 
 export const TENANT_FOLDERS = {
-    Automatico: "1ZaXQB0TGDL4Qcb8Miz_gS_mBvD1w5Fuy",
-    //Manual: "1_p4CdzPWEaWz3NYywhbNTNNpwAgle-5q",
-    PDF_COMPLETO_AUTOMATIZACION: "1_p4CdzPWEaWz3NYywhbNTNNpwAgle-5q"
+    Automatico: process.env.FOLDER_AUTOMATICO,
+    PDF_COMPLETO_AUTOMATIZACION: process.env.FOLDER_PDF_COMPLETO
 };
 
 export const SYSTEM_FOLDERS = {
-    ENTRADA: "1P7AgsYFh6XL_FK8Ww1WLC1KjQ8Jq39oB",
-    TRANSITO: "1f6DEUzg5-D-NraANU5WyYZzeuVCoW7Hc",
-    PROCESADOS: "1yVSIgrK-V3hHR1pjTggjNoRaZ8E-SsLf",
-    ERRORES: "14kfle_EpkJmsFOFS6MbirlaVihPPHrLx"
+    ENTRADA: process.env.FOLDER_ENTRADA,
+    TRANSITO: process.env.FOLDER_ENCOLADO,
+    PROCESADOS: process.env.FOLDER_PROCESADOS,
+    ERRORES: process.env.FOLDER_ERRORES
 };
 
 export const PATHS = {
@@ -20,7 +19,7 @@ export const PATHS = {
     tempImg: process.env.TEMP_IMG_PATH || path.join("tmp", "img"),
     tempPdf: process.env.TEMP_PDF_PATH || path.join("tmp", "pdf"),
     binaries: {
-        // Importante: poppler suele necesitar el ejecutable exacto pdftoppm
+        // poppler suele necesitar el ejecutable exacto pdftoppm
         poppler: process.env.POPPLER_PATH || "C:\\poppler\\Library\\bin\\pdftoppm.exe"
     }
 };
