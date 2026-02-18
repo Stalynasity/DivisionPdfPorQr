5a8fc6ad78e01a00616bdf4892973edfe01c9c59
stalynAsiti <stalynasi08@gmail.com>
2026-02-17 23:42:42 -0500

    comenzar a verificar

---
+++ src/servicioBanco/consultacliente.js
@@ -0,0 +1,19 @@
+
+function procesarYConfirmar(dataRecibida) {
+    try {
+
+        // 1. Lógica de consulta: Verificar si el ID ya existe
+        const existe = dataRecibida.cedula === "1234567890"; // Simulación de consulta
+
+        if (existe) {
+            console.log("Consulta exitosa: El archivo ya se puede registrar.");
+            return true; // Enviamos el true solicitado
+        }
+
+        return false; 
+
+    } catch (error) {
+        console.error("Error procesando data: " + error);
+        return false; 
+    }
+}
